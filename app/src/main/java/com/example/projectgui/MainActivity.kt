9bdb84a21349ad3fc0d81e44054b04d9a672f3c5
Santomen <78631914+Santomen@users.noreply.github.com>
2022-02-26 01:17:42 -0600

    Agrego pantallas de Feedback,watched y wachlist

--- app/src/main/java/com/example/projectgui/MainActivity.kt
+++ app/src/main/java/com/example/projectgui/MainActivity.kt
@@ -5,20 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.twotone.Star
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import com.example.projectgui.ui.theme.ProjectGUITheme
 
 val DarkYellow = Color(0xFFF5C518)
@@ -309,5 +321,272 @@ fun DefaultPreview() {
         //SignUp()
         //LogIn()
         Sidebar()
+    }
+}
+//Santiago
+data class Peliculas(val titulo:String, val year:Int, val imageId:Int=0)
+data class Series(val titulo:String,val year:Int,val imageId:Int=0)
+val lPeliculas= arrayListOf<Peliculas>(Peliculas("Birds of Prey",2020,R.drawable.peli1))
+val lSeries= arrayListOf<Series>(Series("No time to die",2021,R.drawable.serie1))
+fun agregar_peliculas(){
+    lPeliculas.add(Peliculas("Now You see me 2",2016,R.drawable.peli2))
+    lPeliculas.add(Peliculas("Onward",2020,R.drawable.peli3))
+    lPeliculas.add(Peliculas("Mulan",2020,R.drawable.peli4))
+
+}
+@Composable
+fun dropDownMenu() {
+
+    var expanded by remember { mutableStateOf(false) }
+    val suggestions = listOf("Series", "Peliculas")
+    var selectedText by remember { mutableStateOf("") }
+
+    var textfieldSize by remember { mutableStateOf(Size.Zero) }
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+
+    Column(Modifier.padding(20.dp)) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    //This value is used to assign to the DropDown the same width
+                    textfieldSize = coordinates.size.toSize()
+                },
+            label = {Text("Seleccion el tipo de contenido")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){textfieldSize.width.toDp()})
+        ) {
+            suggestions.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    selectedText = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+
+}
+@Composable
+fun Watched(){
+    agregar_peliculas()
+    Column( horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()){
+        Image(
+            painter = painterResource(R.drawable.ojo),
+            contentDescription = "Wachlist",
+            modifier = Modifier
+                .size(175.dp)
+                .padding(top = 80.dp),
+            alignment = Alignment.Center
+        )
+        Text(
+            text = "Wactched",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 25.dp)
+        )
+        //Drop down button
+        Row {
+            dropDownMenu()
+        }
+        Row(){
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = androidx.compose.ui.graphics.Color.Black),
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .size(width = 120.dp, height = 55.dp),
+                shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+            ) {
+                Text(text = "Agregar", color = Color.White, fontSize = 16.sp)
+            }
+            var text by remember{ mutableStateOf("") }
+            TextField(value =text , onValueChange = {}, modifier = Modifier
+                .padding(20.dp)
+                .size(width = 200.dp, height = 55.dp))
+        }
+        listado_contenido()
+
+    }
+}
+
+
+/////////////////////////////////////////////////////////SANTIAGO wachlist
+
+@Composable
+fun Wachlist(){
+    agregar_peliculas()
+    Column( horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()){
+        Image(
+            painter = painterResource(R.drawable.wachlist),
+            contentDescription = "Wachlist",
+            modifier = Modifier
+                .size(175.dp)
+                .padding(top = 80.dp),
+            alignment = Alignment.Center
+        )
+        Text(
+            text = "Wachlist",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 25.dp)
+        )
+
+        listado_contenido()
+
+    }
+
+}
+
+
+@Composable
+fun listado_contenido(){
+
+    LazyColumn(){
+        items(lPeliculas){
+                Peli->Componente_visual(Peli)
+        }
+    }
+}
+@Composable
+fun Componente_visual(peli:Peliculas){
+    Card(
+        shape = MaterialTheme.shapes.small,
+        modifier = Modifier
+            .padding(
+                bottom = 6.dp,
+                top = 6.dp,
+            )
+            .fillMaxWidth(0.5F),
+        elevation = 8.dp,
+    ) {
+
+        Column() {
+            Image(
+                painter = painterResource(peli.imageId),
+                contentDescription = "Wachlist",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.05F),
+                contentScale = ContentScale.Crop,
+            )
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp)
+            ){
+                Text(
+                    text = peli.titulo,
+                    modifier = Modifier
+                        .fillMaxWidth(0.5F)
+                        .wrapContentWidth(Alignment.Start)
+                    ,
+                    style = MaterialTheme.typography.h5
+                )
+                val rank =peli.year.toString()
+                Text(
+                    text = "Año "+rank,
+                    modifier = Modifier
+                        .fillMaxWidth(1F)
+                        .wrapContentWidth(Alignment.End)
+                    ,
+                    style = MaterialTheme.typography.h6
+                )
+
+            }
+        }
+    }
+}
+
+/////Feedback
+@Composable
+fun FeedBack() {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier= Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(20.dp)){
+
+        Image(
+            painter = painterResource(R.drawable.comments),
+            contentDescription = "FeedBack",
+            modifier = Modifier
+                .size(175.dp)
+                .padding(top = 80.dp),
+            alignment = Alignment.Center
+        )
+        Text(
+            text = "FEEDBACK",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 75.dp)
+        )
+        //Image(painter = painterResource(id = comments  ) , contentDescription ="comments" )
+        row_stars()
+        TextAreaExample()
+        Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = androidx.compose.ui.graphics.Color.Black),
+            modifier = Modifier
+                .padding(top = 85.dp)
+                .size(width = 200.dp, height = 55.dp),
+            shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+        ) {
+            Text(text = "Send", color = Color.White, fontSize = 25.sp)
+        }
+    }
+
+}
+
+@Composable
+fun TextAreaExample() {
+    var description by remember { mutableStateOf("Comments") }
+
+    TextField(
+        value = description,
+        onValueChange = { description = it },
+        modifier = Modifier
+            .padding(top = 85.dp)
+            .fillMaxWidth(.7F)
+            .fillMaxHeight(0.5F), shape = RoundedCornerShape(corner = CornerSize(30.dp))
+    )
+}
+@Composable
+fun row_stars(){
+    Row(modifier=Modifier.padding(top=40.dp)){
+        for(x in 1 until 6){
+            Star_buttons()
+        }
+    }
+}
+@Composable
+fun Star_buttons(){
+    IconButton(onClick = {
+
+    }) {
+        Icon(imageVector = Icons.TwoTone.Star,contentDescription = null, modifier = Modifier.size(100.dp))
     }
 }
